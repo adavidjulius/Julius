26b0dce4730eb39000ac99e0d127a7299a78d368
David Julius <davidjulius246@gmail.com>
2026-03-11 19:35:08 +0530

    Delete airllm_loader.py

--- airllm_loader.py
+++
@@ -1,9 +0,0 @@
-from airllm import AutoModel
-
-model = AutoModel.from_pretrained(
-    "mistralai/Mistral-7B-Instruct-v0.1"
-)
-
-def generate_response(prompt):
-    response = model.generate(prompt)
-    return response
